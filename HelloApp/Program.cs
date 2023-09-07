c5b1a9893a027124b3eb0e4bafe8b1c85b4951d0
Yarik22 <popov.yarik.popov@gmail.com>
2023-09-07 20:39:41 +0300

    Добавьте файлы проекта.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var myCompany = new Company
+{
+    Name = "TrackEnsure",
+    StaffAmount = 1502
+};
+
+app.MapGet("/", () =>
+{
+    var companyInfo = "My company name is " + myCompany.Name + ". It's staff counts about " + myCompany.StaffAmount + " workers";
+    return companyInfo;
+});
+
+app.MapGet("/randint", () =>
+{
+    var randomizer = new Random();
+    var randomNumber = randomizer.Next(0, 101);
+
+    return "Your random number is " + randomNumber + ". Congrats!!!";
+});
+app.Use(async (context, next) =>
+{
+    var password = context.Request.Query["password"];
+    if (password == "2204")
+    {
+        await next();
+    }
+    else
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Invalid password");
+    }
+});
+app.Run();
+
+public class Company
+{
+    public string? Name { get; set; }
+    public int StaffAmount { get; set; }
+}
